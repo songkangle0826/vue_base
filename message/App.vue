9d7c8c6e9ded7952591b863ca1fd544582b81f66
songkangle <songkangle@trioly.com>
2020-03-21 13:47:45 +0800

    Message的封装

---
+++ message/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        #app
+
+
+        <button @click="show">弹层</button>
+
+    </div>
+</template>
+<script>
+    import { Message } from './Message/Message.js'
+    export default{
+        methods:{
+            show(){
+                Message.success({
+                    message: '你好',
+                    duration: 3000
+                })
+            }
+        }
+    }
+</script>
---
+++ message/Message/Message.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="skl-message">
+        弹框
+    </div>
+</template>
+<script>
+    export default{
+        // 每次用户点击按钮时 都是自动增加数据  自动渲染到视图上
+        data(){
+            return{
+                layers: [],
+            }
+        },
+        methods: {
+            add(options){
+                this.layers.push(options)
+            }
+        }
+    }
+</script>
